c1224b48dfb56f456962e8cd861b730ed77f710a
seyoung Ko <tpdud3256@gmail.com>
2023-12-21 03:19:08 +0900

    기본 버튼 UI 수정

--- src/components/Button/DefaultButton.js
+++ src/components/Button/DefaultButton.js
@@ -3,4 +3,6 @@ import styled from "styled-components";
 export const DefaultButton = styled.button`
     /* background-color: red;
     color: white; */
+    background-color: gray;
+    color: white;
 `;
